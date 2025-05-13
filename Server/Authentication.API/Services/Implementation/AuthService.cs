375d8354b66470b6f07ba09657d68708a3f42c07
Massamba505 <sabelomaphalala131@gmail.com>
2025-05-13 16:52:44 +0200

    Login Service

--- Server/Authentication.API/Services/Implementation/AuthService.cs
+++ Server/Authentication.API/Services/Implementation/AuthService.cs
@@ -1,8 +1,37 @@
-﻿using Authentication.API.Services.Interfaces;
+﻿using Authentication.API.Models.Domain;
+using Authentication.API.Models.Dto;
+using Authentication.API.Repository.Implementations;
+using Authentication.API.Repository.Interfaces;
+using Authentication.API.Services.Interfaces;
 
 namespace Authentication.API.Services.Implementation
 {
-    public class AuthService : IAuthService
+    public class AuthService(IPeopleRepository peopleRepository) : IAuthService
     {
+        private readonly IPeopleRepository _peopleRepository = peopleRepository;
+
+        public async Task<Person?> LoginAsync(PersonDto person)
+        {
+            var duplicateEmail = await _peopleRepository.GetUserByEmailAsync(person.Email);
+            
+            if(duplicateEmail is null)
+            {
+                return null;
+            }
+
+            var duplicateUsername = await _peopleRepository.GetUserByUsernameAsync(person.Username);
+
+            if (duplicateUsername is null)
+            {
+                return null;
+            }
+
+            await _peopleRepository.CreateUserAsync(person);
+        }
+
+        public Task<Person?> RegisterAsync(PersonDto person)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
